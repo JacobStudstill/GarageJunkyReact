09ee1c7340ea043619d228d05108c2dbfc96881b
Jacob Studstill <jacobstudstill@Jacobs-MacBook-Pro.local>
2024-02-14 10:00:41 -0500

    Finished setup for importing, ports, middleware, and listener for server/app.js

--- server/app.js
+++ server/app.js
@@ -1,9 +1,28 @@
 // import
+const express =require("express")
+const mongoose = require("mongoose")
+const morgan = require("morgna")
+const cors = require("cors")
+require("dovenv").config() 
+
+
+// app
+const app = express()
+
 
 // db
 
+
 // middleware
+app.use(morgan("dev"))
+app.use(cors({ origin: true, credentials: true}))
 
 // routes
 
 // port
+
+const port = process.env.PORT || 8080
+
+// listener
+const server = app.listen(port, () => 
+console.log(`Server is running on port ${port}`))
